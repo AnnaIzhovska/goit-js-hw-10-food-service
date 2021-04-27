aa6e39b67898c18796c26a697260a81eb8e5557e
Anna <72921518+AnnaIzhovska@users.noreply.github.com>
2021-04-27 15:59:27 +0200

    Закончила правки

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 
 import galleryMarkup from './templates/galerry.hbs'
 import cards from './menu.json'
-import './js/localstorage.js'
 import './styles.css'
 
-// refs = {
-
-// }
-
-window.onload = () => {
-const container = document.querySelector('. js-menu');
-
-container.innerHTML = galleryMarkup(cards);
+const refs = {
+  container: document.querySelector('.menu'),
+  themeSwitch: document.querySelector('.theme-switch__control'),
+  body: document.querySelector('body'),
+  input: document.getElementById('theme-switch-toggle'),
 }
 
+// --------------------------- Галерея -----------------------------
+
+window.onload = () => {
+  const makeGalleryRow = cards.map(galleryMarkup).join('');
+  refs.container.insertAdjacentHTML('afterbegin', makeGalleryRow);
+
+}
+
+// --------------------------- Тема -----------------------------
 
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-  // console.log(galleryMarkup(cards));
+  function changeThemeGallery() {
+      if (refs.input.checked) {
+          refs.body.classList.remove(Theme.LIGHT);
+          refs.body.classList.add(Theme.DARK);
+          localStorage.setItem('Theme', Theme.DARK);
+          return;
+      }
+      refs.body.classList.remove(Theme.DARK);
+      refs.body.classList.add(Theme.LIGHT);
+      localStorage.setItem('Theme', Theme.LIGHT);
+  }
 
-  // function createGalleryCardsMarkup(cards){
-  //   return cards
-  //   .map(galleryMarkup)
-  //   .join('');
-  // }
-
-  // document.body.insertAdjacentHTML('beforeend',createGalleryCardsMarkup)
+  function GalleryThemeMenu() {
+   if (localStorage.getItem('Theme') === 'dark-theme') {
+      refs.body.classList.add(Theme.DARK);
+      refs.input.checked = true;
+  }
+}
+refs.input.addEventListener('click', changeThemeGallery);
+GalleryThemeMenu();
